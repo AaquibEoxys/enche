dbb3f9ebc53d9749562f53a2cd98def4da68ea07
Aaquib <aaquib@eoxysitsolutionllp.com>
2023-01-04 09:20:01 +0530

    validation login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import SubHeader from "../../components/subHead/SubHeader";
 import Images from "../../components/Images/Images";
 import { Link } from "react-router-dom";
 import "./login.scss";
 
 const Login = () => {
+  const [values, setValues] = useState(null);
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    if (
+      (values?.[e.target.name]?.length === undefined ||
+        values?.[e.target.name]?.length === 0) &&
+      e.target.value == " "
+    ) {
+      setValues({ ...values, [e.target.name]: "" });
+      setErrors({
+        ...errors,
+        [e.target.name]: `Field is required`,
+      });
+    } else {
+      setValues({ ...values, [e.target.name]: e.target.value });
+      setErrors({ ...errors, [e.target.name]: false });
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    let errorExist = false;
+    let errorsObject = {};
+
+    if (
+      values?.email === "" ||
+      values?.email === null ||
+      values?.email === undefined
+    ) {
+      errorsObject.email = "Please enter email";
+      errorExist = true;
+    } else if (!values?.email.trim()) {
+      errorsObject.email = "Email is required";
+      errorExist = true;
+    } else if (
+      !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values?.email)
+    ) {
+      errorsObject.email = "Please type a valid email address";
+      errorExist = true;
+    }
+    if (
+      values?.password === "" ||
+      values?.password === null ||
+      values?.password === undefined
+    ) {
+      errorsObject.password = "Please enter password";
+      errorExist = true;
+    } else if (values?.password.length < 6) {
+      errorsObject.password = "Password must be longer then 6 character";
+      errorExist = true;
+    }
+
+    if (errorExist) {
+      setErrors(errorsObject);
+      return false;
+    }
+  };
+  console.log(" email", errors)
+
   return (
     <>
       <SubHeader />
@@ -28,14 +88,16 @@ const Login = () => {
               <span className="login_inpSvg">
                 <img src={Images?.gmail} alt="" className="google_logo" />
               </span>
-              <input type="text" placeholder="Enter username" />
+              <input type="text" placeholder="Enter username" name="email" value={values?.email} onChange={(e) => handleChange(e)} />
             </div>
+            <p> {errors?.email} </p>
             <div className="loginInp">
               <span className="login_inpSvg">
                 <img src={Images?.lock} alt="" className="google_logo" />
               </span>
-              <input type="text" placeholder="Password" />
+              <input type="password" placeholder="Password" name="password" value={values?.password} onChange={(e) => handleChange(e)} />
             </div>
+            <p> {errors?.password} </p>
             <div className="remberDev">
               <input type="checkbox" id="rememberMe" />
               <label htmlFor="rememberMe" className="remberMe">
@@ -43,7 +105,7 @@ const Login = () => {
               </label>
             </div>
             <div>
-              <button className="login_btn"> Log in </button>
+              <button className="login_btn" onClick={onSubmit}> Log in </button>
             </div>
             <div className="notYet_div">
               <div className="not_yet_text"> Not member yet? </div>
