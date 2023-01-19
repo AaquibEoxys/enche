970448f9ae52d26ba1782ee7ad42cf5be528d9a1
Sanket Sahoo <118505779+sanketeoxys@users.noreply.github.com>
2023-01-19 10:31:22 -0800

    rectify conflict

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ import AboutEnche from "./components/Footer/FooterPages/AboutEnche";
 import AboutEnchepreneur from "./components/Footer/FooterPages/AboutEnchepreneur";
 import AllProducts from "./Pages/ProductPages/AllProductsPages/AllProduct/AllProducts";
 import ProductDetails from "./Pages/ProductPages/ProductDetailsPage/Product/ProductDetails";
-
 import Buyer from "./Pages/Dashboard/components/Buyer";
 import ForgotPassword from "./Pages/ForgotPassword/ForgotPassword";
 import Cart from "./Pages/Cart/Cart";
+import Checkout from "./Pages/Checkout/Checkout";
 function App() {
   return (
     <HashRouter>
@@ -30,11 +30,11 @@ function App() {
         <Route path="/aboutenche" element={<AboutEnche />} />
         <Route path="/aboutenchepreneur" element={<AboutEnchepreneur />} />
         <Route path="/allproducts" element={<AllProducts />} />
-        {/* <Route path="/buyer-dashboard" element={<BuyerDashboard />} /> */}
         <Route path="/productdetails" element={<ProductDetails />} />
         <Route path="/buyer-dashboard" element={<Buyer />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
       <Footer />
     </HashRouter>
